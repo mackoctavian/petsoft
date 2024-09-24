726ee96ca5d80023e17d3915f9b03ea7af5394b0
Mack Octavian <maestrooctavian@gmail.com>
2024-09-25 00:18:08 +0300

    Added shadcn button

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,7 @@
 import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -21,7 +23,14 @@ export default function Home() {
           Use petsoft to easily keep track of pets under your care. Get lifetime
           access for $299
         </p>
-        <div className="mt-10"></div>
+        <div className="mt-10 space-x-3">
+          <Button asChild>
+            <Link href="/signup">Get started</Link>
+          </Button>
+          <Button variant="secondary" asChild>
+            <Link href="/login">Log in</Link>
+          </Button>
+        </div>
       </div>
     </main>
   );
