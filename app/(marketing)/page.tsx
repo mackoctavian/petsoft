7d91eb252c1519aec06a49dd9e84fd7c1443e900
Mack Octavian <maestrooctavian@gmail.com>
2024-09-24 23:41:15 +0300

    layout the homepage

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,28 @@
+import Logo from "@/components/logo";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Test</h1>
+    <main className="flex md:flex-row flex-col items-center justify-center gap-10 bg-[#5dc9a8] min-h-screen">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
+        alt="preview"
+        width={519}
+        height={472}
+      />
+
+      <div>
+        <Logo />
+        <h1 className="text-5xl font-semibold my-6 max-w-[500px]">
+          Manage your <span className="font-extrabold">pet daycare</span> with
+          ease
+        </h1>
+        <p className="text-2xl font-medium max-w-[600px]">
+          Use petsoft to easily keep track of pets under your care. Get lifetime
+          access for $299
+        </p>
+        <div className="mt-10"></div>
+      </div>
     </main>
   );
 }
