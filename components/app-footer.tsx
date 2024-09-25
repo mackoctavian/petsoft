26c13700a6ed38b0728b0b76e4717d0db3384bce
Mack Octavian <maestrooctavian@gmail.com>
2024-09-25 22:50:59 +0300

    Added a sticky footer

--- components/app-footer.tsx
+++ components/app-footer.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function AppFooter() {
-  return <div>AppFooter</div>;
+  return (
+    <footer className="mt-auto border-t border-black/5 px-4">
+      <small className="opacity-50">
+        &copy; 2030 Mack Octavian. All rights reserved.
+      </small>
+    </footer>
+  );
 }
