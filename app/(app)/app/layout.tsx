67dbf29ccaf6d39986265e1affb83068a1db2f82
Mack Octavian <maestrooctavian@gmail.com>
2024-09-26 01:52:03 +0300

    Added a contextAPI for pets

--- app/(app)/app/layout.tsx
+++ app/(app)/app/layout.tsx
@@ -1,15 +1,25 @@
 import AppFooter from "@/components/app-footer";
 import AppHeader from "@/components/app-header";
 import BackgroundPattern from "@/components/background-button";
+import PetContextProvider from "@/contexts/pet-context-provider";
 import React, { ReactNode } from "react";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default async function Layout({ children }: { children: ReactNode }) {
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
+  );
+  if (!response.ok) {
+    throw new Error("Could not fetch pets");
+  }
+
+  const data = await response.json();
+
   return (
     <>
       <BackgroundPattern />
       <div className="flex flex-col max-w-[1050px] mx-auto px-4 min-h-screen">
         <AppHeader />
-        {children}
+        <PetContextProvider data={data}>{children}</PetContextProvider>
         <AppFooter />
       </div>
     </>
---
+++ contexts/pet-context-provider.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React, { createContext, useState } from "react";
+
+export const PetContext = createContext(null);
+
+export default function PetContextProvider({ children, data }) {
+  const [pets, setPets] = useState(data);
+  const [selectedPetId, setSelectedPetId] = useState(null);
+  return (
+    <PetContext.Provider value={{ pets, selectedPetId }}>
+      {children}
+    </PetContext.Provider>
+  );
+}
